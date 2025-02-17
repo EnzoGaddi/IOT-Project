cb8b2938ae43ea30d45494009bf7d33b299190c6
EnzoGaddi <150245454+EnzoGaddi@users.noreply.github.com>
2025-02-17 18:10:08 +0000

    Add files via upload

--- Direction.ino
+++ Direction.ino
@@ -21,25 +21,26 @@ void reverse()
   controlMotor1(200, false);
   controlMotor2(200, false);
 
-  delay(2000);
+  delay(3000);
 
   controlMotor1(220, false);
   controlMotor2(220, false);
 }
 
-void stop()
+void stop() 
 {
-  digitalWrite(M1, LOW);  // No direction for Motor 1
   digitalWrite(M2, LOW);  // No direction for Motor 2
-  ledcWrite(PWM_CHANNEL_E1, 0); // Stop Motor 1
+  digitalWrite(M1, LOW);  // No direction for Motor 1
+
   ledcWrite(PWM_CHANNEL_E2, 0); // Stop Motor 2
+  ledcWrite(PWM_CHANNEL_E1, 0); // Stop Motor 1
 }
 
 void slowDown()
 {
   controlMotor1(220, true);
   controlMotor2(220, true);
-
+ 
   delay(2000);
 
   controlMotor1(200, true);
--- Engine.ino
+++ Engine.ino
@@ -1,8 +1,8 @@
 // Define ESP32 outputs to the motor driver pins
-#define M1  5   // Motor 1 control pin (Direction)
-#define E1  17  // Motor 1 enable pin (PWM for speed)
-#define M2  14  // Motor 2 control pin (Direction)
-#define E2  27  // Motor 2 enable pin (PWM for speed)
+#define M1  14  // Motor 1 control pin (Direction)
+#define E1  16  // Motor 1 enable pin (PWM for speed)
+#define M2  5   // Motor 2 control pin (Direction)
+#define E2  17  // Motor 2 enable pin (PWM for speed)
 
 // PWM parameters
 #define PWM_FREQ     5000 // Frequency for PWM
@@ -10,10 +10,9 @@
 #define PWM_CHANNEL_E1 0  // PWM channel for Motor 1 (E1)
 #define PWM_CHANNEL_E2 1  // PWM channel for Motor 2 (E2)
 
-// Initialize motor control
 void setup() 
 {
-  //Serial.begin(115200);
+  Serial.begin(9600);
 
   // Set M1 and M2 as output for direction control
   pinMode(M1, OUTPUT);
@@ -37,26 +36,23 @@ void setup()
   delay(2000);
 
   USSsetup();
-  ACC_setup();
+  ACCsetup();
 }
 
 // Function to control Motor 1
-void controlMotor1(int speed, bool forward) 
-{
+void controlMotor1(int speed, bool forward) {
   digitalWrite(M1, forward ? HIGH : LOW);  // Set direction
   ledcWrite(PWM_CHANNEL_E1, speed);       // Set speed
 }
 
 // Function to control Motor 2
-void controlMotor2(int speed, bool forward) 
-{
+void controlMotor2(int speed, bool forward) {
   digitalWrite(M2, forward ? HIGH : LOW);  // Set direction
   ledcWrite(PWM_CHANNEL_E2, speed);       // Set speed
 }
 
 void loop() 
 {
-  topSpeed();
   avoidCrash();
-  ACC_loop();
+  ACCloop();
 }
